6329759360886434b74958a3d4880c1deaf0dc5f
davidtang <95396906+dtang123@users.noreply.github.com>
2022-09-11 13:51:45 -0400

    added contact page and emailJS integration. Need to perfect the email formatting

--- src/Routes/contact/contact.component.jsx
+++ src/Routes/contact/contact.component.jsx
@@ -1,6 +1,42 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { ContactContainer } from './contact.styles';
+import emailjs from 'emailjs-com';
+
+
 const Contact = () => {
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_5trxnxh', 'template_v0gvpll', e.target, 'XyHOUIZQ-cgoRnpZX')
+      .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          window.location.reload()   
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
-    <h1>This is the contact page</h1>
+    <ContactContainer>
+      <h1>Contact Me</h1>
+      <Form className="email-form" onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="from_email" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control placeholder="Enter subject" name="subject" required/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={8} aria-label="With textarea" name="message" required/>
+        </Form.Group>
+        <Button type="submit">Send</Button>
+      </Form>
+    </ContactContainer>
   )
 }
 
---
+++ src/Routes/contact/contact.styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { PageContainer } from '../about-me/about.styled';
+
+
+export const ContactContainer = styled(PageContainer) `
+    & .email-form {
+        margin-top: 30px;
+        Button {
+            margin-top: 30px;
+        }
+    }
+`
