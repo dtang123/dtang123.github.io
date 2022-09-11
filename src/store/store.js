1bd99968e080ee747ff60d3582643e07e8646d3c
davidtang <95396906+dtang123@users.noreply.github.com>
2022-09-11 15:37:39 -0400

    Updates

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import ProjectSlice  from './projects/projects.reducer'
-
-export const store = configureStore({
-    reducer: {
-        projectSlice: ProjectSlice
-    }
-})
